effcbec7cf72cd21fa53db81b27d3c7bff224d2a
ravikanthdba <rgarimel@rgarimel-mn3.linkedin.biz>
2021-12-27 19:22:00 +0530

    Change text

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here to Learn React
         </a>
       </header>
     </div>
